5f36fd0147f6d9741750a83f39bf8ba0c44be0f3
Siraj Yesuf <sirajyesuf762@gmail.com>
2025-01-12 20:21:25 +0300

    =hero bg updated

--- components/hero.tsx
+++ components/hero.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const Hero = () => {
   return (
-    <section className="pt-20 md:pt-48 pb-8 md:pb-12 px-4 sm:px-6 lg:px-8 ] bg-[#f2f3f2] md:min-h-[80vh] ">
+    <section className="pt-20 md:pt-48 pb-8 md:pb-12 px-4 sm:px-6 lg:px-8 ] bg-[#f2f3f2] md:min-h-[80vh] bg-gradient-to-br from-blue-50 via-blue-100 to-white rounded-3xl">
       <div className="max-w-7xl mx-auto text-[#214186]">
         <div className="text-center">
           <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold tracking-tight mb-4 md:mb-6">
@@ -18,7 +18,7 @@ const Hero = () => {
             <Button
               asChild
               size="lg"
-              className="w-full sm:w-auto bg-blue-500 text-white  font-bold rounded-lg hover:bg-blue-800"
+              className="w-full sm:w-auto bg-blue-500 text-white font-bold rounded-lg hover:bg-blue-600"
             >
               <Link href="/#service_section">Explore Services</Link>
             </Button>
@@ -26,7 +26,7 @@ const Hero = () => {
               asChild
               size="lg"
               variant="outline"
-              className="w-full sm:w-auto bg-[#214186] text-white font-bold"
+              className="w-full sm:w-auto  text-blue-500 font-bold"
             >
               <Link href="/contactus">Contact Us</Link>
             </Button>
