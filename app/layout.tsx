70c6b0ea95faac9171fb18807fc0c97724c47834
Siraj Yesuf <sirajyesuf762@gmail.com>
2025-01-22 13:09:02 +0300

    =google metatag added

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,6 +42,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="Cqz7vb2pBilwmVi2GmmIJZtA662R9kjTkC63uoq8KTM"
+        />
+      </head>
       <body>
         <Navbar />
         <main className="pt-20">{children}</main>
