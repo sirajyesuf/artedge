dc41f17a8e1dd575ecbc2e0ea353ab63a99eda46
Siraj Yesuf <sirajyesuf762@gmail.com>
2025-01-24 20:17:33 +0300

    =metadata typo fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,34 +5,33 @@ import Footer from "@/components/footer";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://artedgesecurityandcleaning.com"),
-  title: "Artech Edge Security and Cleaning Services",
+  title: "ArtEdge Security and Cleaning Services",
   description:
-    "Artech Edge Security and Cleaning Services is a leading provider of security and cleaning services in Ethiopia.",
+    "ArtEdge Trading is a leading provider of security and cleaning services in Ethiopia.",
   keywords:
-    "security, cleaning, janitorial, professional development, guarding, surveillance, cleaning supplies, training, security uniforms, PPE, facility management, Ethiopia services",
-  authors: [{ name: "Artech Edge" }],
+    "security, cleaning, janitorial, professional development, guarding, surveillance, cleaning supplies, training, security uniforms, PPE, facility management,Ethiopia,AddisAbaba",
+  authors: [{ name: "ArtEdge" }],
   openGraph: {
     type: "website",
     locale: "en_US",
     url: "https://artedgesecurityandcleaning.com",
-    siteName: "Artech Edge Security and Cleaning Services",
+    siteName: "ArtEdge Security and Cleaning Services",
     images: [
       {
         url: "https://artedgesecurityandcleaning.com/artedgelogo.jpg",
         width: 1200,
         height: 630,
-        alt: "Artech Edge Security and Cleaning Services",
+        alt: "ArtEdge Security and Cleaning Services",
       },
     ],
   },
   twitter: {
     card: "summary_large_image",
-    title: "Artech Edge Security and Cleaning Services",
+    title: "ArtEdge Security and Cleaning Services",
     description:
-      "Artech Edge Security and Cleaning Services is a leading provider of security and cleaning services in Ethiopia.",
+      "ArtEdge Trading is a leading provider of security and cleaning services in Ethiopia.",
     images: ["https://artedgesecurityandcleaning.com/artedgelogo.jpg"],
   },
-  themeColor: "#0863b2",
 };
 
 export default function RootLayout({
