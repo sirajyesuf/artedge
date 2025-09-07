15f31f89d554f9e562199b114e91ad10f44920e8
siraj yesuf <sirajyesuf762@gmail.com>
2025-09-08 01:05:30 +0300

    =duplicate removed

--- app/data/newsData.ts
+++ app/data/newsData.ts
@@ -133,32 +133,4 @@ export const sidebarNews: NewsItem[] = [
     ],
     url: "#",
   },
-
-  {
-    id: 4,
-    title: "ArtEdge 2024 Annual Meeting",
-    images: [
-      {
-        url: "/newsphoto/2024_1.jpg",
-        caption: "photo one",
-      },
-      {
-        url: "/newsphoto/2024_2.jpg",
-        caption: "photo two",
-      },
-      {
-        url: "/newsphoto/2024_3.jpg",
-        caption: "photo three",
-      },
-      {
-        url: "/newsphoto/2024_4.jpg",
-        caption: "photo four",
-      },
-      {
-        url: "/newsphoto/2024_5.jpg",
-        caption: "photo five",
-      },
-    ],
-    url: "#",
-  },
 ];
